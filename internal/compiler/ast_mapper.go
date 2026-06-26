8bf8c820039467702b89f45d77bdc86c52dfc653
Jordan Paul <jordan@getpixeltag.com>
2026-06-26 08:17:19 -0700

    fix: remove go-tree-sitter CGO dependency in AST mapper

--- internal/compiler/ast_mapper.go
+++ internal/compiler/ast_mapper.go
@@ -1,21 +1,21 @@
 package compiler
 
 import (
-	"context"
+	"bytes"
 	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
 	"os"
 	"path/filepath"
 	"strings"
-
-	sitter "github.com/smacker/go-tree-sitter"
-	"github.com/smacker/go-tree-sitter/golang"
 )
 
 // GenerateMap creates a static graph context representation of the Go workspace
-// by parsing source code with Tree-sitter and extracting key declarations.
+// by parsing source code with native go/parser and extracting key declarations.
 func GenerateMap(workspaceRoot string) (string, error) {
 	var sb strings.Builder
-	lang := golang.GetLanguage()
 
 	// Directories to scan (ignore noisy ones)
 	targetDirs := []string{"cmd", "internal", "pkg"}
@@ -41,22 +41,48 @@ func GenerateMap(workspaceRoot string) (string, error) {
 				return nil
 			}
 
-			content, err := os.ReadFile(path)
+			// Parse using Go standard library parser
+			fset := token.NewFileSet()
+			file, err := parser.ParseFile(fset, path, nil, parser.ParseComments)
 			if err != nil {
-				return nil // Skip files we can't read
+				return nil // Skip files that fail to parse
 			}
 
-			parser := sitter.NewParser()
-			parser.SetLanguage(lang)
-			tree, _ := parser.ParseCtx(context.Background(), nil, content)
-
-			if tree == nil {
-				return nil
-			}
+			var fileSigs []string
+			ast.Inspect(file, func(n ast.Node) bool {
+				if n == nil {
+					return true
+				}
+				switch x := n.(type) {
+				case *ast.TypeSpec:
+					var typeKind string
+					switch x.Type.(type) {
+					case *ast.StructType:
+						typeKind = "struct"
+					case *ast.InterfaceType:
+						typeKind = "interface"
+					default:
+						var buf bytes.Buffer
+						if printer.Fprint(&buf, fset, x.Type) == nil {
+							typeKind = buf.String()
+						}
+					}
+					if typeKind != "" {
+						fileSigs = append(fileSigs, fmt.Sprintf("type %s %s", x.Name.Name, typeKind))
+					}
+				case *ast.FuncDecl:
+					// Create a shallow copy to clear the body for printing signature only
+					declCopy := *x
+					declCopy.Body = nil
+					var buf bytes.Buffer
+					if printer.Fprint(&buf, fset, &declCopy) == nil {
+						fileSigs = append(fileSigs, buf.String())
+					}
+				}
+				return true
+			})
 
 			relPath, _ := filepath.Rel(workspaceRoot, path)
-			fileSigs := extractSignatures(tree.RootNode(), content)
-
 			if len(fileSigs) > 0 {
 				sb.WriteString(fmt.Sprintf("\n### File: %s\n", relPath))
 				for _, sig := range fileSigs {
@@ -73,63 +99,4 @@ func GenerateMap(workspaceRoot string) (string, error) {
 	}
 
 	return sb.String(), nil
-}
-
-func extractSignatures(root *sitter.Node, content []byte) []string {
-	var sigs []string
-
-	// Helper to extract node text
-	nodeText := func(n *sitter.Node) string {
-		if n == nil {
-			return ""
-		}
-		return string(content[n.StartByte():n.EndByte()])
-	}
-
-	// We'll use simple tree traversal for now instead of complex queries
-	var traverse func(node *sitter.Node)
-	traverse = func(node *sitter.Node) {
-		if node == nil {
-			return
-		}
-		switch node.Type() {
-		case "type_declaration":
-			// A type declaration might have multiple type specs, but let's just grab the whole line (up to newline or '{')
-			text := nodeText(node)
-			firstLine := strings.Split(text, "{")[0]
-			firstLine = strings.Split(firstLine, "\n")[0]
-			sigs = append(sigs, strings.TrimSpace(firstLine))
-		case "function_declaration":
-			nameNode := node.ChildByFieldName("name")
-			paramsNode := node.ChildByFieldName("parameters")
-			if nameNode != nil && paramsNode != nil {
-				sig := fmt.Sprintf("func %s%s", nodeText(nameNode), nodeText(paramsNode))
-				// Add return type if present
-				resNode := node.ChildByFieldName("result")
-				if resNode != nil {
-					sig += " " + nodeText(resNode)
-				}
-				sigs = append(sigs, sig)
-			}
-		case "method_declaration":
-			receiverNode := node.ChildByFieldName("receiver")
-			nameNode := node.ChildByFieldName("name")
-			paramsNode := node.ChildByFieldName("parameters")
-			if receiverNode != nil && nameNode != nil && paramsNode != nil {
-				sig := fmt.Sprintf("func %s %s%s", nodeText(receiverNode), nodeText(nameNode), nodeText(paramsNode))
-				resNode := node.ChildByFieldName("result")
-				if resNode != nil {
-					sig += " " + nodeText(resNode)
-				}
-				sigs = append(sigs, sig)
-			}
-		}
-
-		for i := 0; i < int(node.ChildCount()); i++ {
-			traverse(node.Child(i))
-		}
-	}
-
-	traverse(root)
-	return sigs
 }
